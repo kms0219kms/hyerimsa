00f99b3db22ae21c44290e44be2d8f6837f89dde
All PGS <kms0219kms@naver.com>
2021-03-19 20:33:38 +0900

    오류 수정

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import { Navbar, Footer, NotFound } from './components';
 function App() {
   return (
     <div>
-      <Navbar />
+      <div className="small-device">
+        <center>
+            <h3>죄송합니다.</h3><br />
+            <h5>서비스를 이용하는데 필요한 최소한의 기기 사이즈가 아닙니다.</h5>
+            <h5>서비스를 이용하려면, 가로 320px 이상이어야 합니다.</h5>
+            <h5>* 갤럭시 폴드의 경우 화면을 펼쳐 이용해 주세요.</h5>
+        </center>
+      </div>
       <div className="App">
+      <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Redirect from="/Home" to="/" component={Home} />
@@ -17,8 +25,8 @@ function App() {
           <Route path="/React" component={React_main} />
           <Route component={NotFound} />
         </Switch>
-      </div>
       <Footer />
+      </div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,20 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-        <div className="index-medium">
-            <span className="index-topic">혜림사</span>
-            <span className="index-desc"></span>
-            안녕하세요. <br />충청북도 단양군에 위치한 소백산 혜림사입니다.<br /><br />
-        </div>
+            <div className="mobile">
+                <div className="index-medium">
+                    <span className="index-topic">혜림사</span>
+                    <span className="index-desc"></span>
+                    안녕하세요. <br />충청북도 단양군에 위치한 소백산 혜림사입니다.<br /><br />
+                </div>
+            </div>
+            <div className="pc">
+                <div className="index-full">
+                    <span className="index-topic">혜림사</span>
+                    <span className="index-desc"></span>
+                    안녕하세요. <br />충청북도 단양군에 위치한 소백산 혜림사입니다.<br /><br />
+                </div>
+            </div>
         </div>
     );
 };
